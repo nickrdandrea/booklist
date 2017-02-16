b3eb2a8d22c68c7758018c946520df02be59e234
nickrdandrea <nickrdandrea@gmail.com>
2017-02-15 19:11:14 -0500

    Add CSV response.

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,13 @@
 class BooksController < ApplicationController
   def index
-    respond_to :html, :text
+    @books = Book.all
+    respond_to do |format|
+      format.html
+      format.text
+      format.csv do |books|
+        books = Book.generate_csv(@books)
+        render :plain => books
+      end
+    end
   end
 end
